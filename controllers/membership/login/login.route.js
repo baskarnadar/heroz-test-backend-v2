e44b5adfce4cc278e41c63dfd4fb80f657082615
baskarnadar <baskarnadar@gmail.com>
2026-03-18 05:13:43 +0300

    Initial upload

--- controllers/membership/login/login.route.js
+++ controllers/membership/login/login.route.js
@@ -6,6 +6,6 @@ const router = express.Router();
 
 router.post("/memsignup", MemLogin.memsignup);
 router.post("/getmemdata", protectAPI,MemLogin.getmemdata);
- 
+ router.post("/updatememdata", protectAPI,MemLogin.updatememdata);
 
 module.exports = router;
